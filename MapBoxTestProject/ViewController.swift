9d728378c6573a638dd4c68fd5eb7d7deaf84ef3
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-07-05 15:37:59 +0300

    first init project;

---
+++ MapBoxTestProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MapBoxTestProject
+//
+//  Created by Ruslan Yupyn on 7/5/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MGLMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'MapBoxTestProject' do
+  pod 'Mapbox-iOS-SDK', '~> 3.2.3'
+end
